a9b76a62cfd4bbde20d1ffa84d08d1812cea4806
Lê Văn Mong <49313037+Mongker@users.noreply.github.com>
2023-08-23 01:29:03 +0700

    Build

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_1 = __importDefault(require("cors"));
+const helmet_1 = __importDefault(require("helmet"));
+dotenv_1.default.config();
+const PORT = parseInt(process.env.PORT, 10);
+const app = (0, express_1.default)();
+/**
+ *  App Configuration
+ */
+app.use((0, cookie_parser_1.default)());
+app.use((0, helmet_1.default)());
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.static('build'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/build/index.html');
+});
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.listen(PORT, () => {
+    console.log(`⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
