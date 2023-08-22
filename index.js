f8a3181c0af049966ba3c94f6d51d1f6a93f5167
Lê Văn Mong <49313037+Mongker@users.noreply.github.com>
2023-08-23 02:08:27 +0700

    Di ngu

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const cors_1 = __importDefault(require("cors"));
-const helmet_1 = __importDefault(require("helmet"));
-dotenv_1.default.config();
-const PORT = parseInt(process.env.PORT, 10) || 3000;
-const app = (0, express_1.default)();
+/**
+ * Copyright 2020 present, Lê Văn Mong.
+ * All rights reserved.
+ * @author Mong_Le_Van on 09/09/2020
+ * @email: levanmong.dola.99@gmail.com
+ * @student-code: 68DCHT20091
+ * @university: UTT (Đại học Công Nghệ Giao Thông Vận Tải)
+ */
+
+require('dotenv').config();
+// server.js
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 1999;
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 /**
  *  App Configuration
  */
-app.use((0, cookie_parser_1.default)());
-app.use((0, helmet_1.default)());
-app.use((0, cors_1.default)());
-app.use(express_1.default.json());
-app.use(express_1.default.static('build'));
+app.use(express.static('build'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + 'build/index.html');
 });
